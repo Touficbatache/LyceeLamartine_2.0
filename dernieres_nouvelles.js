f4529439c1a3f6530b2de87cec0d7d8f263a1f7a
Toufic Batache <taffou2a@gmail.com>
2017-09-25 18:04:20 +0300

    Update dernieres_nouvelles.js

--- dernieres_nouvelles.js
+++ dernieres_nouvelles.js
@@ -1,5 +1,5 @@
 var nouvelles = [{
   title: "Fermeture de l'école demain",
-  content: "Pour l'instant, rien n'a été déclaré par l'école a propos de la fermeture du Mardi 26-09-2017.",
+  content: "Pour l'instant, l'école n'a rien déclaré à propos de la fermeture du Lycee le Mardi 26-09-2017 mais il semble qu'il y aura grève. Toutefois, il y a des chances que ça change et aucune information n'est sûre.",
   creation_date: "2017-09-25"
 }];
