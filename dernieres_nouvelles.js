4c79c03f6da1504bd2eb2d57746e8a1c6daf0f3d
Toufic Batache <taffou2a@gmail.com>
2017-10-20 20:30:52 +0300

    Update dernieres_nouvelles.js

--- dernieres_nouvelles.js
+++ dernieres_nouvelles.js
@@ -1,5 +1,5 @@
 var nouvelles = [{
-  title: "Fermeture de l'école demain",
-  content: "L'école sera ouverte et les transports seront assurés le Mercredi 27 Septembre 2017.",
-  creation_date: "26/09/2017"
+  title: "Pas de nouvelles importantes",
+  content: "Aucune nouvelle importante pour le moment.",
+  creation_date: ""
 }];
